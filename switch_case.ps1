855987c5ebec5d7121f05d36e849b3e115f7dffb
Futchick <magomedov307@gmail.com>
2026-03-24 19:49:01 +0300

    my change

---
+++ switch_case.ps1
@@ -0,0 +1,6 @@
+﻿$value = 10
+switch ($value){
+5 {Write-Host "Value is 5" -ForegroundColor Red}
+10 {Write-Host "Value is 10" -ForegroundColor Green}
+default {Write-Host "Value is somthing else"}
+}
