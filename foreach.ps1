6f0af4189729913e154e285d5f790f6e707691e7
Futchick <magomedov307@gmail.com>
2026-03-24 21:45:21 +0300

    my change

---
+++ foreach.ps1
@@ -0,0 +1,9 @@
+﻿$x=@("1","2","3","4")
+foreach($i in $x) {
+    if(($i -lt 2) -or ($i -eq 2)) {
+        Write-Host "$i is Green" -ForegroundColor Green 
+    }
+    else { 
+        Write-Host "$i is Yellow" -ForegroundColor Yellow
+    }
+}
