981a36a49984738c257aa9782ad474b3079e4982
Futchick <magomedov307@gmail.com>
2026-03-25 22:06:39 +0300

    my change

---
+++ foreach-object.ps1
@@ -0,0 +1,10 @@
+﻿$x=@("1","2","3","4")
+$x | ForEach-Object{
+
+    if($_ -lt 2){
+        Write-Host "$_ is Green" -ForegroundColor Green
+    }
+    else {
+        Write-Host "$_ is Yellow" -ForegroundColor Yellow
+    }
+}
