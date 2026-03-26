21a7ecbe1b3cacb89beaddd4f5c19d6a523a3029
Futchick <magomedov307@gmail.com>
2026-03-26 21:42:58 +0300

    my change

---
+++ for.ps1
@@ -0,0 +1,8 @@
+﻿for($x=1; $x -le 5; $x++){
+    if($x -lt 2){
+        Write-Host "$x is Green" -ForegroundColor Green
+        }
+    else{
+        Write-Host "$x is Yellow" -ForegroundColor Yellow
+        }
+    }
