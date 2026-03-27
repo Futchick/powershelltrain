c07c0b8c7a348ef7aa9ca66307af3ceb5f7b324c
Futchick <magomedov307@gmail.com>
2026-03-27 09:30:23 +0300

    my change

---
+++ while.ps1
@@ -0,0 +1,9 @@
+﻿$x=0
+while($x -ne 4){$x++
+    if($x -lt 2){
+    Write-Host "$x is Green" -ForegroundColor Green
+    }
+    else{
+        Write-Host "$x is Yellow" -ForegroundColor Yellow
+    }
+}
