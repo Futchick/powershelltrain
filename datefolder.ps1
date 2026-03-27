eded2bf62ced357d29501a33b18f2246e3317967
Futchick <magomedov307@gmail.com>
2026-03-27 16:20:20 +0300

    my change

---
+++ datefolder.ps1
@@ -0,0 +1,3 @@
+﻿$Dname = ((Get-Date).AddDays(0).ToString('yyyyMMdd')) #date manipulation
+$dirName = "ConfigBackup_$Dname" # prefix for the folder
+New-Item -Path C:\Windows\Temp -Name $dirName -ItemType directory
