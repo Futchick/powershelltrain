4ba064dcdb71c1af1eaf85516d5622a192ef3a36
Futchick <magomedov307@gmail.com>
2026-03-27 09:16:04 +0300

    my change

---
+++ doWhile.ps1
@@ -0,0 +1,9 @@
+﻿$x=0
+do{$x++
+    if($x -lt 2){
+    Write-Host "$x is Green" -ForegroundColor Green
+    }
+    else{
+    Write-Host "$x is Yellow" -ForegroundColor Yellow
+    }
+}while($x -ne 4)
